6921b79ef6f2dbee681339c9c3bfa8301354d446
nm <nm@gmail.com>
2025-06-12 22:29:18 +0200

    ueb02

---
+++ ueb02/ab1/ab1.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+*
+* Aufgabe 1
+*	
+* Der Benutzer gibt ein Interval an, dabei muss das Programm auf das Zahleninterval reagieren
+* folgende Eigenschaften sollen geprüft werden:
+*		Ob die Zahl ein gerade Zahl ist,
+*		Ob die Zahl eine primenzahl ist,
+*		Ob die Zahl eine perfekte Zahl ist 
+*
+* Dabei wird die Zahl nur ausgegeben wenn eine der Eigenschaften zutrifft.
+* Zusätzlich muss noch der Interval überprüft werden ob er gültig ist, (start <= end)
+*	
+* Beispiel:
+*   > 0 
+*   > 6
+*	0 : gerade
+*   2 : gerade, primenzahl,
+*	3 : primenzahl
+*   4 : gerade
+*   5 : primenzahl
+*	6 : perfekt, gerade 
+*
+* Unerlaubte Mittel: Funktionen, Arrays, Strings 
+*/
+func main() {
+	
+}	
---
+++ ueb02/ab2/ab2.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+
+Aufgabe 2
+
+- Programmieren sie eine Zahlenrate Spiel.
+
+Das Programm beginnt mit den Fragen wo sich die Zahl befinden kann.
+
+Hilfe: frage = (obergrenze - untergrenze) / 2 + untergrenze 
+
+Beispiel Gesucht 42: 
+	Untergrenze: 0
+	Obergrenze: 100
+	Ist deine Zahlgrösser als 50? (j:1,n:0) 0
+	Ist deine Zahlgrösser als 25? (j:1,n:0) 1
+	Ist deine Zahlgrösser als 38? (j:1,n:0) 1
+	Ist deine Zahlgrösser als 44? (j:1,n:0) 0
+	Ist deine Zahlgrösser als 41? (j:1,n:0) 1
+	Ist deine Zahlgrösser als 43? (j:1,n:0) 0
+	Ist deine Zahlgrösser als 42? (j:1,n:0) 0
+	Deine Zahl ist 42
+*/
+func main() {
+	
+}
