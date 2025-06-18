a0a08f29c7670c3c98e353ec010642f94e23e288
nm <nm@gmail.com>
2025-06-18 18:25:49 +0200

    ueb02

--- ueb02/ab1/ab1.go
+++ ueb02/ab1/ab1.go
@@ -1,31 +1,38 @@
 package main
 
-/**
-*
-* Aufgabe 1
-*	
-* Der Benutzer gibt ein Interval an, dabei muss das Programm auf das Zahleninterval reagieren
-* folgende Eigenschaften sollen geprüft werden:
-*		Ob die Zahl ein gerade Zahl ist,
-*		Ob die Zahl eine primenzahl ist,
-*		Ob die Zahl eine perfekte Zahl ist 
-*
-* Dabei wird die Zahl nur ausgegeben wenn eine der Eigenschaften zutrifft.
-* Zusätzlich muss noch der Interval überprüft werden, ob diese gültig ist (start <= end)
-* und dementsprechend folgt eine geeignete Fehlerbehandlung.
-*
-* Beispiel:
-*   > 0 
-*   > 6
-* 0: gerade,
-* 2: gerade, primenzahl,
-* 3: primenzahl,
-* 4: gerade,
-* 5: primenzahl,
-* 6: gerade, perfekt,
-*
-* Unerlaubte Mittel: Funktionen, Arrays, Strings
- */
-func main() {
+
+/*
+	Aufgabe 1
+
+	Programmieren sie ein Mini-Rechner!
+
+	Das Programm beginnt erstmals mit eine Benutzereingabe, welches die Rechenoperation aufwählt.
+	Danach wird der Benutzer erneut befragt, welche Operanden er für die Berechnung verwenden will.
+
+	Das Programm läuft solange bis der Benutzer keine Berechnung mehr machen will.
+
+	Welche Datentypen sie letzendlich für die Berechnung nehmen ist ihnen freigestellt.
+
+	Beispiel:
+	Willkommen beim Mini-Rechner!
+
+	Wähle eine Operation:
+	1 - Addition
+	2 - Subtraktion
+	3 - Multiplikation
+	4 - Division
+	> 3
+
+	Gib die erste Zahl ein: 5
+	Gib die zweite Zahl ein: 4
+	Ergebnis: 20
+
+	Noch eine Berechnung? (y:1/n:0): 1
 	
+	Wähle eine Operation:
+	...
+*/
+
+func main() {
+
 }
